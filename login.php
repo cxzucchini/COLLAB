b12ea9e191ee4993a6a9c1ab66bf4fa475706b3a
gewaswas <kylebryandavid1@gmail.com>
2025-08-12 10:39:39 +0800

    Update login.php

--- login.php
+++ login.php
@@ -6,3 +6,14 @@ if (isset($_POST['logout'])) {
     header('Location: index.html');
     exit;
 }
+
+$valid_users = [
+    'johndoe@gmail.com' => 'password123'
+];
+
+$error_message = '';
+$success_message = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && !isset($_POST['logout'])) {
+    $email = trim($_POST['email']);
+    $password = $_POST['password'];
